3f930c986985d24bf69c55b39fd7d4237a01d4b8
bobo <boboyag@126.com>
2017-09-08 10:42:37 +0800

    前台页面获取信息的接口

---
+++ server/news.php
@@ -0,0 +1,58 @@
+<?php 
+
+    header("content-type:application/json;charset=utf-8");
+
+    require_once('db.php');
+
+// 检测连接
+    // if (!$conn) {
+    //     die("Connection failed: " . mysqli_connect_error());
+    // }
+    // echo "Connected successfully";
+
+    if ($link) {
+        
+        //执行成功的过程
+        if ($_GET['newstype']) {
+            $newstype = $_GET['newstype'];
+            $sql = "SELECT * FROM `news` WHERE `newstype` = '{$newstype}'";
+            mysqli_query($link,'set names utf8');
+            $result = mysqli_query($link,$sql);
+            $senddata = array();
+            while ($row = mysqli_fetch_assoc($result)) {
+                array_push($senddata,array( 'id'=>$row['id'],
+                                            'newstype'=>$row['newstype'],
+                                            'newstitle'=>$row['newstitle'],
+                                            'newsimg'=>$row['newsimg'],
+                                            'newssrc'=>$row['newssrc'],
+                                            'newstime'=>$row['newstime']
+                            ));
+            }
+            echo json_encode($senddata,JSON_UNESCAPED_UNICODE);
+        }else{
+            $sql = 'SELECT * FROM news';
+            mysqli_query($link,'set names utf8');
+            $result = mysqli_query($link,$sql);
+            $senddata = array();
+            while ($row = mysqli_fetch_assoc($result)) {
+                array_push($senddata,array( 'id'=>$row['id'],
+                                            'newstype'=>$row['newstype'],
+                                            'newstitle'=>$row['newstitle'],
+                                            'newsimg'=>$row['newsimg'],
+                                            'newssrc'=>$row['newssrc'],
+                                            'newstime'=>$row['newstime']
+                            ));
+            }
+            echo json_encode($senddata,JSON_UNESCAPED_UNICODE);
+        }    
+
+    }else{
+        echo json_encode(array('success'=>'none'));
+    }
+
+
+    // $arr = array('newstitle'=>'新闻标题','newsimg'=>'images/3.jpg','newstime'=>'2017-3-17','newssrc'=>'极客科技');
+    
+    mysqli_close($link);
+    // echo json_encode($arr);
+?>
